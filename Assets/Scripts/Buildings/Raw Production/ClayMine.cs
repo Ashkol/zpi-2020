ac2d0c1d36292da1a771b082f97598f2d5389903
Kacper Wilczkiewicz <wilczek180@op.pl>
2020-04-24 23:51:54 +0200

    Building resources production

--- Assets/Scripts/Buildings/Raw Production/ClayMine.cs
+++ Assets/Scripts/Buildings/Raw Production/ClayMine.cs
@@ -4,5 +4,55 @@ using UnityEngine;
 
 public class ClayMine : RawProductionBuilding
 {
-    
+    [Header("Chain links")]
+    public Brickyard nextInChain;
+	
+	void Update()
+    {
+        if (timeSinceLastProduction == 0f && nextInChain)
+            StartCoroutine(Produce());
+        if (currentResources.clay >= producedResources.clay && timeSinceLastPass == 0f && nextInChain)
+            StartCoroutine(PassResources());
+    }
+	
+	IEnumerator Produce()
+    {
+        while(timeSinceLastProduction < productionTime)
+        {
+            timeSinceLastProduction += Time.deltaTime;
+            productionProgress = timeSinceLastProduction / productionTime;
+            yield return null;
+        }
+        timeSinceLastProduction = 0f;
+        productionProgress = timeSinceLastProduction / productionTime;
+        currentResources += producedResources;
+    }
+
+    IEnumerator PassResources()
+    {
+        currentResources -= producedResources;
+        while (timeSinceLastPass < passProductTime)
+        {
+            timeSinceLastPass += Time.deltaTime;
+            passProgress = timeSinceLastPass / passProductTime;
+            yield return null;
+        }
+        timeSinceLastPass = 0f;
+        passProgress = timeSinceLastPass / passProductTime;
+        nextInChain.currentResources += producedResources;
+    }
+
+    public override bool CheckForNeighbouringBuildings()
+    {
+        Debug.Log("Checks for Brickyard");
+        List<Brickyard> chainBuildings = GetNeighbouringBuildings<Brickyard>();
+        if (nextInChain == null && chainBuildings.Count >= 1)
+        {
+            Debug.Log(">= 1");
+            nextInChain = chainBuildings[0];
+            return true;
+        }
+
+        return false;
+    }
 }
--- Assets/Scripts/Buildings/Raw Production/StoneMine.cs
+++ Assets/Scripts/Buildings/Raw Production/StoneMine.cs
@@ -4,5 +4,56 @@ using UnityEngine;
 
 public class StoneMine : RawProductionBuilding
 {
-    
+    [Header("Chain links")]
+    public Storehouse nextInChain;
+	
+	void Update()
+    {
+        if (timeSinceLastProduction == 0f && nextInChain)
+            StartCoroutine(Produce());
+        if (currentResources.stone >= producedResources.stone && timeSinceLastPass == 0f && nextInChain)
+            StartCoroutine(PassResources());
+    }
+	
+	    IEnumerator Produce()
+    {
+        while(timeSinceLastProduction < productionTime)
+        {
+            timeSinceLastProduction += Time.deltaTime;
+            productionProgress = timeSinceLastProduction / productionTime;
+            yield return null;
+        }
+        timeSinceLastProduction = 0f;
+        productionProgress = timeSinceLastProduction / productionTime;
+        currentResources += producedResources;
+    }
+
+    IEnumerator PassResources()
+    {
+        Debug.Log("Passing resources stone mine -> storehouse");
+        currentResources -= producedResources;
+        while (timeSinceLastPass < passProductTime)
+        {
+            timeSinceLastPass += Time.deltaTime;
+            passProgress = timeSinceLastPass / passProductTime;
+            yield return null;
+        }
+        timeSinceLastPass = 0f;
+        passProgress = timeSinceLastPass / passProductTime;
+        nextInChain.AddResourcess(producedResources);
+    }
+
+    public override bool CheckForNeighbouringBuildings()
+    {
+        Debug.Log("Checks for Storehouse");
+        List<Storehouse> chainBuildings = GetNeighbouringBuildings<Storehouse>();
+        if (nextInChain == null && chainBuildings.Count >= 1)
+        {
+            Debug.Log(">= 1");
+            nextInChain = chainBuildings[0];
+            return true;
+        }
+
+        return false;
+    }
 }
