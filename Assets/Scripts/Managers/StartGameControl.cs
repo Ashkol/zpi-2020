5045235ce2e991b88325510f16cd209718be5d61
Adam Szkolny <adamszkolny@gmail.com>
2020-05-19 15:50:09 +0200

    Add Master Scene and music

---
+++ Assets/Scripts/Managers/StartGameControl.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGameControl : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        FindObjectOfType<GameManager>().LoadScene((int)SceneIndexes.GAME);
+    }
+}
