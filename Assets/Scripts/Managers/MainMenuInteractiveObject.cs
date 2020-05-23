62883b4a47e046f276f4983e1ee6489fcb83ccc7
Adam Szkolny <adamszkolny@gmail.com>
2020-05-23 16:47:18 +0200

    Add quit interactive board

---
+++ Assets/Scripts/Managers/MainMenuInteractiveObject.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class MainMenuInteractiveObject : MonoBehaviour
+{
+    GameManager gameManager;
+
+    public UnityEvent OnMouseDownEvent;
+
+    private void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    private void OnMouseDown()
+    {
+        OnMouseDownEvent.Invoke();
+        //gameManager.LoadScene((int)SceneIndexes.GAME);
+    }
+
+    public void LoadScene(int sceneIndex)
+    {
+        gameManager.LoadScene(sceneIndex);
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        // Application.Quit() does not work in the editor so
+        // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+         Application.Quit();
+#endif
+    }
+}
