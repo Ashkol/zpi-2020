2d220cfbfff78f9a5f67fd053493c1a34d296f22
Kacper Wilczkiewicz <wilczek180@op.pl>
2020-06-08 22:30:53 +0200

    Residents progress bar

--- Assets/Scripts/UI/ResidentialBuildingPanel.cs
+++ Assets/Scripts/UI/ResidentialBuildingPanel.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+
 public class ResidentialBuildingPanel : BuildingPanel
 {
     private ResidentialBuilding building;
@@ -24,16 +26,30 @@ public class ResidentialBuildingPanel : BuildingPanel
 
 	public TextMeshProUGUI bodyCount;
 	public GameObject updateButton;
+	public Image fill;
+	public Color color;
 	
 	void Update()
 	{
 		bodyCount.text = "Residents number: " + building.getResidents();
 		if(building.updateConditionsMet() && !updateButton.activeSelf) updateButton.SetActive(true);
 		else if(!building.updateConditionsMet() && updateButton.activeSelf) updateButton.SetActive(false);
+		
+		getCurrentFill();
 	}
 	
 	public void upgrade(){
 	
 		building.updateToBetterHouse();
+	}
+	
+	void getCurrentFill(){
+	
+		float fillAmount = (float) building.getResidents() / (float) building.rasidentsMax;
+		
+		RectTransform rt = fill.GetComponent<RectTransform>();
+		rt.localScale = new Vector3(fillAmount, 1, 1);
+		
+		fill.color = color;
 	}
 }
