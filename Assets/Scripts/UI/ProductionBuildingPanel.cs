d7dc1c4424e69ef589b6018f6e4c3b5dc287ef69
Kacper Wilczkiewicz <wilczek180@op.pl>
2020-06-08 18:33:36 +0200

    Production progress bar

--- Assets/Scripts/UI/ProductionBuildingPanel.cs
+++ Assets/Scripts/UI/ProductionBuildingPanel.cs
@@ -2,13 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+
 public class ProductionBuildingPanel : BuildingPanel
 {
-    [SerializeField] ProductionBuilding building;
+    private ProductionBuilding building;
 
+	public new ProductionBuilding Building
+    {
+        get { return building; }
+        set
+        {
+            building = value;
+            header.text = building.description.name;
+            description.text = building.description.description;
+        }
+    }
+
+	public Image fill;
+	public Color color;
+	
     protected override void Start()
     {
         base.Start();
     }
-
+	
+	void Update(){
+		
+		getCurrentFill();
+	}
+	
+	void getCurrentFill(){
+	
+		float fillAmount = building.productionProgress;
+		
+		RectTransform rt = fill.GetComponent<RectTransform>();
+		rt.localScale = new Vector3(fillAmount, 1, 1);
+		
+		fill.color = color;
+	}
 }
